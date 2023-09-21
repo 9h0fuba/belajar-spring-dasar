29eb2760ecd01f174f5150703c91b4a624b8a96d
ghofuba <ghofuba@gmail.com>
2023-09-21 22:30:12 +0700

    @Import: memungkinkan Configuration class di import dalam mainconfiguration lalu secara manual dimasukan satu per satu, jadi tidak perlu membuat application.Context satu per satu

---
+++ src/main/java/ghofuba/belajarspringdasar/MainConfiguration.java
@@ -0,0 +1,14 @@
+package ghofuba.belajarspringdasar;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(value = {
+        FooConfiguration.class,
+        BarConfiguration.class
+})
+public class MainConfiguration {
+
+
+}
